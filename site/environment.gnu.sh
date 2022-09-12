bca743f9e8e03c75553e6354a0556c66ee105493
Lauren Chilutti <Lauren.Chilutti@noaa.gov>
2022-09-12 13:14:47 -0400

    moving module load cmake to site specific environment file, adding cmake flag in BUILDnceplibs to avoid a compilation issue with systems using a standard less than C99, fixing one more reference to the location of libFMS

--- site/environment.gnu.sh
+++ site/environment.gnu.sh
@@ -35,6 +35,7 @@ if [ `hostname | cut -c1-4` = "gaea" ] || [ `hostname | cut -c1-3` = "nid" ] ; t
    module load gcc/9.2.0
    module load cray-netcdf
    module load craype-hugepages4M
+   module load cmake/3.20.1
 
    # make your compiler selections here
    export FC=ftn
@@ -58,6 +59,7 @@ elif [ `hostname | cut -c1-5` = "Orion" ] ; then
    module load impi/2021.2
    module load netcdf
    module load hdf5
+   module load cmake/3.22.1
 
    export CPATH="${NETCDF}/include:${CPATH}"
    export HDF5=${HDF5_ROOT}
@@ -87,6 +89,7 @@ elif [ `hostname | cut -c1-2` = "fe" ] || [ `hostname | cut -c1` = "x" ] ; then
    module load impi/2020
    module load hdf5/1.10.5
    module load netcdf4/4.7.2
+   module load cmake/3.20.1
 
    export LIBRARY_PATH="${LIBRARY_PATH}:${NETCDF4}/lib:${HDF5}/lib"
    export NETCDF_DIR=${NETCDF4}
@@ -110,6 +113,7 @@ elif [ `hostname | cut -c1` = "h" ] ; then
    module load impi/2020
    module load netcdf/4.7.2
    module load hdf5/1.10.5
+   module load cmake/3.20.1
 
    export LIBRARY_PATH="${LIBRARY_PATH}:${NETCDF}/lib:${HDF5}/lib"
    export NETCDF_DIR=${NETCDF}
@@ -136,6 +140,7 @@ elif [ `hostname | cut -c1-3` = "lsc" ] ; then
    module load impi/2020
    module load netcdf/4.8.0
    module load hdf5/1.12.0
+   module load cmake/3.18.2
 
    export CPATH="${NETCDF_ROOT}/include:${CPATH}"
    export NETCDF_DIR=${NETCDF_ROOT}
